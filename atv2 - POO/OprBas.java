303d172b1e99ce53de12b7b116a04edadc0a6a33
luma <lumadesouzaferreira1@gmail.com>
2025-08-13 09:08:40 -0300

    Add files via upload

---
+++ atv2 - POO/OprBas.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class OprBas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite dois números");
+       
+    }
+}
